613c53d7b2ab69a1db70a1b18ce062c670f78256
YusufOnatYilmaz <yusufonatyilmaz@gmail.com>
2022-09-22 01:34:10 +0300

    text creation

--- README.md
+++ README.md
@@ -2,7 +2,10 @@
 Notion - discord integration for daily task reminder.
 
 ## TODO
-- [ ] Retrieve Database
+- [x] Create the output text
+- [x] Retrieve Database
 - [ ] create yaml
+- [ ] Discord bot
+- [ ] Deploy bot
 
 Yusuf Onat Yılmaz
--- read_database.py
+++ read_database.py
@@ -3,19 +3,31 @@ import json
 
 from constants import DATABASE_ID, FILTERS, NOTION_VERSION, BEARER_TOKEN
 
-url = f"https://api.notion.com/v1/databases/{DATABASE_ID}/query"
-
 payload = json.dumps(FILTERS)
 
 headers = {
-  'Authorization': f'Bearer {BEARER_TOKEN}',
-  'Content-Type': 'application/json',
-  'Notion-Version': NOTION_VERSION
+    'Authorization': f'Bearer {BEARER_TOKEN}',
+    'Content-Type': 'application/json',
+    'Notion-Version': NOTION_VERSION
 }
 
-response_text = requests.request("POST", url, headers=headers, data=payload)
-response = json.loads(response_text.text)
+text = ""
+for key in DATABASE_ID:
 
-for result in response["results"]:
-    name = result["properties"]["Name"]["title"][0]["plain_text"]
-    tags =  [multi_select["name"]  for multi_select in result["properties"]["Type"]["multi_select"]]
+    url = f"https://api.notion.com/v1/databases/{DATABASE_ID[key]}/query"
+
+    response_text = requests.request("POST", url, headers=headers, data=payload)
+    response = json.loads(response_text.text)
+
+    text += f"{key}:\n"
+    for result in response["results"]:
+        
+        name = result["properties"]["Name"]["title"][0]["plain_text"]
+        tags =  [multi_select["name"]  for multi_select in result["properties"]["Type"]["multi_select"]]
+        
+        text += f"- {name}"
+        for tag in tags:
+            text += f" ({tag})"
+        text += "\n"
+
+print(text)
